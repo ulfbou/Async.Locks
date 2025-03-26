8f776df9ba5fe9537b83517a20814197ed21b4fc
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-03-26 10:11:45 +0100

    Add InternalsVisibleTo attribute for Async.Locks.Tests
    - Added InternalsVisibleTo attribute to allow the test project Async.Locks.Tests to access internal members of the Async.Locks assembly.
    - This change facilitates better unit testing by enabling access to internal methods and properties.

---
+++ Src/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿// Copyright (c) Async Framework projects. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Async.Locks.Tests")]
