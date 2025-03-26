4deeca75c79188cd6b5244dfa2c4861154061c2b
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-03-26 10:18:46 +0100

    Initial commit for IAsyncReaderWriterLock interface and ReaderWriterLockEventArgs class
    - Added IAsyncReaderWriterLock interface to define asynchronous reader-writer lock operations.
      - Includes methods for acquiring read, write, and upgradeable read locks.
      - Defines events for lock acquisition and release.
    - Added ReaderWriterLockEventArgs class to provide event arguments for reader-writer lock events.
      - Includes a property to get the ID of the thread that acquired or released the lock.
    - These additions provide the foundation for implementing asynchronous reader-writer lock functionality in the Async.Locks library.

---
+++ Src/IAsyncReaderWriterLock.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Async Framework projects. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async.Locks
+{
+    /// <summary>
+    /// Represents an asynchronous reader-writer lock.
+    /// </summary>
+    public interface IAsyncReaderWriterLock : IAsyncLock
+    {
+        /// <summary>
+        /// Asynchronously acquires a read lock.
+        /// </summary>
+        /// <param name="cancellationToken">Optional cancellation token to cancel the operation.</param>
+        /// <returns>A value task that represents the acquisition of the read lock. The result of the task is an <see cref="IAsyncDisposable"/> that releases the lock when disposed.</returns>
+        ValueTask<IAsyncDisposable> ReadLockAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Asynchronously acquires a write lock.
+        /// </summary>
+        /// <param name="cancellationToken">Optional cancellation token to cancel the operation.</param>
+        /// <returns>A value task that represents the acquisition of the write lock. The result of the task is an <see cref="IAsyncDisposable"/> that releases the lock when disposed.</returns>
+        ValueTask<IAsyncDisposable> WriteLockAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Asynchronously acquires an upgradeable read lock.
+        /// </summary>
+        /// <param name="cancellationToken">Optional cancellation token to cancel the operation.</param>
+        /// <returns>A value task that represents the acquisition of the upgradeable read lock. The result of the task is an <see cref="IAsyncDisposable"/> that releases the lock when disposed.</returns>
+        ValueTask<IAsyncDisposable> UpgradeableReadLockAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Occurs when a read lock is acquired.
+        /// </summary>
+        event EventHandler<ReaderWriterLockEventArgs> ReadLockAcquired;
+
+        /// <summary>
+        /// Occurs when a write lock is acquired.
+        /// </summary>
+        event EventHandler<ReaderWriterLockEventArgs> WriteLockAcquired;
+
+        /// <summary>
+        /// Occurs when a read lock is released.
+        /// </summary>
+        event EventHandler<ReaderWriterLockEventArgs> ReadLockReleased;
+
+        /// <summary>
+        /// Occurs when a write lock is released.
+        /// </summary>
+        event EventHandler<ReaderWriterLockEventArgs> WriteLockReleased;
+    }
+}
