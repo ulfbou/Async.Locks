f43fe8f2a08049682182daf2f56ac2d986b0b318
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-03-27 11:11:05 +0100

    feat: Add TimeoutAfter extension methods for Task and ValueTask
    This commit introduces `TimeoutAfter` extension methods for both `Task` and `ValueTask`, allowing developers to easily add timeouts to asynchronous operations.
    
    The added methods are:
    
    - `TaskExtensions.TimeoutAfter<T>(this Task<T> task, TimeSpan timeout)`
    - `TaskExtensions.TimeoutAfter(this Task task, TimeSpan timeout)`
    - `ValueTaskExtensions.TimeoutAfter<T>(this ValueTask<T> task, TimeSpan timeout)`
    - `ValueTaskExtensions.TimeoutAfter(this ValueTask task, TimeSpan timeout)`
    
    These methods use `Task.WhenAny` and `Task.Delay` to implement the timeout logic, throwing a `TimeoutException` if the task does not complete within the specified timeout.
    
    This provides a convenient and consistent way to handle timeouts in asynchronous code.

---
+++ Tests/TaskExtensions.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Async Framework projects. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace Async.Locks.Tests
+{
+    public static class TaskExtensions
+    {
+        public static async Task<T> TimeoutAfter<T>(this Task<T> task, TimeSpan timeout)
+        {
+            if (await Task.WhenAny(task, Task.Delay(timeout)) == task)
+            {
+                return await task;
+            }
+            else
+            {
+                throw new TimeoutException("The operation has timed out.");
+            }
+        }
+
+        public static async Task TimeoutAfter(this Task task, TimeSpan timeout)
+        {
+            if (await Task.WhenAny(task, Task.Delay(timeout)) == task)
+            {
+                await task;
+            }
+            else
+            {
+                throw new TimeoutException("The operation has timed out.");
+            }
+        }
+    }
+}
---
+++ Tests/ValueTaskExtensions.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Async Framework projects. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace Async.Locks.Tests
+{
+    public static class ValueTaskExtensions
+    {
+        public static async ValueTask TimeoutAfter(this ValueTask task, TimeSpan timeout)
+        {
+            if (await Task.WhenAny(task.AsTask(), Task.Delay(timeout)) == task.AsTask())
+            {
+                await task;
+            }
+            else
+            {
+                throw new TimeoutException("The operation has timed out.");
+            }
+        }
+
+        public static async ValueTask<T> TimeoutAfter<T>(this ValueTask<T> task, TimeSpan timeout)
+        {
+            if (await Task.WhenAny(task.AsTask(), Task.Delay(timeout)) == task.AsTask())
+            {
+                return await task;
+            }
+            else
+            {
+                throw new TimeoutException("The operation has timed out.");
+            }
+        }
+    }
+}
