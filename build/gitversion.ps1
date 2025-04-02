b77dd112312f0d27451492a72e8e0c2a681e5983
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-04-02 17:35:44 +0200

    feat: Add gitversion.ps1 to generate version properties
    This commit adds `gitversion.ps1`, a PowerShell script that uses the GitVersion tool to generate version properties for the build process.
    
    The script performs the following actions:
    
    - Executes `dotnet gitversion /output json` to retrieve version information from GitVersion.
    - Converts the JSON output to PowerShell objects using `ConvertFrom-Json`.
    - Generates `gitversion.props` file containing the `AssemblyVersion`, `FileVersion`, and `InformationalVersion` properties based on the GitVersion output.
    
    This script automates the generation of version properties, ensuring that the build uses consistent and accurate version information derived from Git history.

---
+++ build/gitversion.ps1
@@ -0,0 +1,4 @@
+$gitVersionOutput = dotnet gitversion /output json | ConvertFrom-Json
+"AssemblyVersion=$($gitVersionOutput.AssemblySemVer)" | Out-File gitversion.props
+"FileVersion=$($gitVersionOutput.SemVer)" | Out-File gitversion.props -Append
+"InformationalVersion=$($gitVersionOutput.InformationalVersion)" | Out-File gitversion.props -Append
